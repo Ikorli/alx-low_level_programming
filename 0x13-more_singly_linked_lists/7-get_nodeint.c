c9dba7ea21201a473a4f52c639bde3f98487cf95
tarila <ikorlit@gmail.com>
2023-04-05 16:01:33 -0700

    updated

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -7,12 +7,13 @@
  *
  * Return: pointer to the node or NULL
  */
- listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 unsigned int i;
 listint_t *temp;
 
-for (i = 0, temp = head; temp && i < index; i++, temp = temp->next);
+for (i = 0, temp = head; temp && i < index; i++, temp = temp->next)
+	;
 
 return (temp ? temp : NULL);
 }
