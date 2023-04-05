ef5fad58aec435603002d1b9ed6b55689aa8a2bc
tarila <ikorlit@gmail.com>
2023-04-05 16:52:21 -0700

    updated

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,37 +1,30 @@
+#include "lists.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include "lists.h"
 
 /**
- * print_listint_safe - Prints a listint_t list safely.
- * @head: A pointer to the head of the listint_t list.
+ * print_listint_safe - prints a listint_t linked list
+ * @head: pointer to the head of the list
  *
- * Return: The number of nodes in the list.
+ * Return: number of nodes in the list
  */
 size_t print_listint_safe(const listint_t *head)
 {
-size_t count = 0;
-const listint_t *slow = head;
-const listint_t *fast = head;
+	const listint_t *temp;
+	size_t count;
 
-while (fast != NULL && fast->next != NULL)
-{
-count++;
-printf("[%p] %d\n", (void *)slow, slow->n);
-slow = slow->next;
-fast = fast->next->next;
-
-if (slow == fast)
-{
-printf("-> [%p] %d\n", (void *)slow, slow->n);
-printf("List is looped\n");
-exit(98);
-}
-}
-if (fast != NULL)
-{
-count++;
-printf("[%p] %d\n", (void *)slow, slow->n);
-}
-return (count);
+	count = 0;
+	while (head != NULL)
+	{
+		count++;
+		printf("[%p] %d\n", (void *)head, head->n);
+		temp = head;
+		head = head->next;
+		if (temp <= head)
+		{
+			printf("-> [%p] %d\n", (void *)head, head->n);
+			break;
+		}
+	}
+	return (count);
 }
