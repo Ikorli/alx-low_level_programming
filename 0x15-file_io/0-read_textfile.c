b4db31efd4921dea722dabfb593a16ef454519c0
ikorli <ikorlit@gmail.com>
2023-07-10 17:10:06 -0700

    updated

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,37 +1,52 @@
 #include "main.h"
 #include <stdlib.h>
-#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 /**
- * read_textfile - Read text file and print to STDOUT.
+ * read_textfile - Read text file and print its contents to STDOUT.
  * @filename: Name of the text file to be read.
  * @letters: Number of letters to be read.
- * Return: Actual number of bytes read and printed.
- * 0 on failure or if filename is NULL.
+ *
+ * Return: Number of bytes read and printed on success, 0 on failure or
+ *         if filename is NULL.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	int fd, bytes_read, bytes_written;
+	char *buffer;
+
 	if (filename == NULL)
 		return (0);
 
-	FILE *file = fopen(filename, "r");
-
-	if (file == NULL)
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
 		return (0);
 
-	char *buf = malloc(sizeof(char) * (letters + 1));
-
-	if (buf == NULL)
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
 	{
-		fclose(file);
+		close(fd);
 		return (0);
 	}
 
-	ssize_t t = fread(buf, sizeof(char), letters, file);
-	ssize_t w = fwrite(buf, sizeof(char), t, stdout);
+	bytes_read = read(fd, buffer, letters);
+	if (bytes_read == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
 
-	free(buf);
-	fclose(file);
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+	if (bytes_written == -1 || bytes_written != bytes_read)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
 
-	return (w);
+	free(buffer);
+	close(fd);
+	return (bytes_written);
 }
